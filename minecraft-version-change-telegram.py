2ce9120d2b53052f04982a6e84d2b90445c62c87
Bixilon <bixilon@bixilon.de>
2021-01-16 14:42:26 +0100

    add gitignore, ci, show more info of new version

--- minecraft-version-change-telegram.py
+++ minecraft-version-change-telegram.py
@@ -4,13 +4,13 @@
 *
 * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 *
-*  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 *
-*  You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
+* You should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>.
 *
-*  This software is not affiliated with Mojang AB, the original developer of Minecraft.
+* This software is not affiliated with Mojang AB, the original developer of Minecraft.
 """
-
+import urllib.parse
 from threading import Timer
 
 import requests
@@ -18,50 +18,61 @@ import requests
 MANIFEST_URL = "https://launchermeta.mojang.com/mc/game/version_manifest.json"
 TELEGRAM_CHAT_ID = '@<chat id>'
 TELEGRAM_BOT_TOKEN = '<token>'
+CHECK_DELAY = 60 * 5
 
 manifest = requests.get(MANIFEST_URL).json()
 
 latestVersionAnnounced = manifest["latest"]["snapshot"]  # assume current latest version was already announced
 
 
-class perpetualTimer():
+class PerpetualTimer:
+    def __init__(self, delay, executor):
+        self.delay = delay
+        self.executorFunction = executor
+        self.timer = Timer(self.delay, self.executor)
 
-    def __init__(self, t, hFunction):
-        self.t = t
-        self.hFunction = hFunction
-        self.thread = Timer(self.t, self.handle_function)
-
-    def handle_function(self):
-        self.hFunction()
-        self.thread = Timer(self.t, self.handle_function)
-        self.thread.start()
+    def executor(self):
+        self.executorFunction()
+        self.timer = Timer(self.delay, self.executor)
+        self.timer.start()
 
     def start(self):
-        self.thread.start()
+        self.timer.start()
 
     def cancel(self):
-        self.thread.cancel()
+        self.timer.cancel()
+
+
+def sendTelegramMessage(message):
+    telegramResponse = requests.get("https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/sendMessage?chat_id=" + TELEGRAM_CHAT_ID + "&parse_mode=markdown&text=%s" % (urllib.parse.quote(message))).json()
+    if not telegramResponse["ok"]:
+        print("Failed to announce message: %s" % telegramResponse["description"])
 
 
 def checkForVersionChange():
-    global latestVersionAnnounced
+    global latestVersionAnnounced, manifest
     print("Checking for new version...")
     manifest = requests.get(MANIFEST_URL).json()
     if manifest["latest"]["snapshot"] == latestVersionAnnounced:
         print("No new version found")
         return
     print("New version found: %s" % manifest["latest"]["snapshot"])
-    newVersion = {}
+    newVersionData = {}
     for version in manifest["versions"]:
         if version["id"] == manifest["latest"]["snapshot"]:
-            newVersion = version
+            newVersionData = version
+
+    versionJson = requests.get(newVersionData["url"]).json()
     # announce in telegram
-    telegramResponse = requests.get("https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/sendMessage?chat_id=" + TELEGRAM_CHAT_ID + "&parse_mode=markdown&text=Mojang just released a new minecraft %s: %s" % (newVersion["type"], newVersion["id"])).json()
-    if not telegramResponse["ok"]:
-        print("Failed to announce message: %s" % telegramResponse["description"])
-    latestVersionAnnounced = newVersion["id"]
+    sendTelegramMessage("""A new minecraft version is available: %s
+Type: `%s`
+Major release: `%s`
+""" % (newVersionData["id"],
+       newVersionData["type"],
+       versionJson["assetIndex"]["id"]))
+    latestVersionAnnounced = newVersionData["id"]
 
 
 print("Starting bot...")
-timer = perpetualTimer(60 * 60, checkForVersionChange)
+timer = PerpetualTimer(CHECK_DELAY, checkForVersionChange)
 timer.start()
